949519c103b7d3118ed5f940ac491da2fd22cfc3
xiongjian <webjsforyou@163.com>
2020-04-22 16:42:11 +0800

    feat: render方法的解析

--- 03.VueComponent/04.Comment/src/index.js
+++ 03.VueComponent/04.Comment/src/index.js
@@ -8,5 +8,10 @@ import App from "./App";
 
 new Vue({
   el: "#app", // public/index.html 对应上
+  // 最终组件的内容渲染到页面元素上~
   render: (h) => h(App),
+  // render: function(createElement) {
+  //   // 自动注册App组件，并返回要渲染的App组件实例对象，最终会渲染到页面el元素上
+  //   return createElement(App);
+  // },
 });
