4a8a571cc2049d111ea8eccec20ef6a3a389e65d
xiongjian <webjsforyou@163.com>
2020-04-27 14:20:46 +0800

    feat: count练习（没有使用vuex）

---
+++ 03.VueComponent/08.vuex/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>clicked: {{ count }} times, count is {{ oddOrEven }}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  computed: {
+    oddOrEven() {
+      return this.count % 2 === 1 ? "奇数" : "偶数";
+    },
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+    incrementIfOdd() {
+      // 判断是否是奇数
+      if (this.count % 2 === 1) {
+        this.count++;
+      }
+    },
+    incrementAsync() {
+      // 模拟异步加法
+      setTimeout(() => {
+        this.count++;
+      }, 1000);
+    },
+  },
+};
+</script>
+
+<style></style>
