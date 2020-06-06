2af09c502411d2016cd7dddbbea6689f10d9cc8c
xiongjian <webjsforyou@163.com>
2020-06-06 10:05:19 +0800

    完成comments练习

--- 02.react/4.react_cli/src/pages/Add/index.jsx
+++ 02.react/4.react_cli/src/pages/Add/index.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addComment } from "../../redux/actions";
 
-export default class Add extends Component {
+// 传递redux添加数据的方法
+@connect(null, { addComment })
+class Add extends Component {
+  state = {
+    name: "",
+    content: "",
+  };
+
+  handleChange = (key) => {
+    return (e) => {
+      this.setState({
+        [key]: e.target.value,
+      });
+    };
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, content } = this.state;
+    if (!name || !content) {
+      alert("输入数据不能为空");
+      return;
+    }
+    this.props.addComment({ name, content, id: Date.now() });
+    this.setState({
+      name: "",
+      content: "",
+    });
+  };
+
   render() {
+    const { name, content } = this.state;
+
     return (
       <div className="col-md-4">
-        <form className="form-horizontal">
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>用户名</label>
-            <input type="text" className="form-control" placeholder="用户名" />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="用户名"
+              onChange={this.handleChange("name")}
+              value={name}
+            />
           </div>
           <div className="form-group">
             <label>评论内容</label>
@@ -15,11 +55,14 @@ export default class Add extends Component {
               className="form-control"
               rows="6"
               placeholder="评论内容"
+              onChange={this.handleChange("content")}
+              value={content}
             ></textarea>
           </div>
           <div className="form-group">
             <div className="col-sm-offset-2 col-sm-10">
-              <button type="button" className="btn btn-default pull-right">
+              {/* submit才能触发onSubmit事件 */}
+              <button type="submit" className="btn btn-default pull-right">
                 提交
               </button>
             </div>
@@ -29,3 +72,4 @@ export default class Add extends Component {
     );
   }
 }
+export default Add;
--- 02.react/4.react_cli/src/pages/List/index.jsx
+++ 02.react/4.react_cli/src/pages/List/index.jsx
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { delComment } from "../../redux/actions";
 
-@connect((state) => ({ comments: state.comments }), {})
+@connect((state) => ({ comments: state.comments }), { delComment })
 class List extends Component {
+  handleClick = (id) => {
+    return () => {
+      if (window.confirm(`您确认要删除当前评论吗~`)) {
+        this.props.delComment(id);
+      }
+    };
+  };
+
   render() {
     // 获取容器组件传递的redux数据
     const { comments } = this.props;
-    
+
     return (
       <div className="col-md-8">
         <h3 className="reply">评论回复：</h3>
-        <h2 style={{ display: "none" }}>暂无评论，点击左侧添加评论！！！</h2>
+        <h2 style={{ display: comments.length ? "none" : "block" }}>
+          暂无评论，点击左侧添加评论！！！
+        </h2>
         <ul className="list-group">
           {comments.map((comment) => {
             return (
               <li className="list-group-item" key={comment.id}>
                 <div className="handle">
-                  <a href="javascript:;">删除</a>
+                  <a href="javascript:;" onClick={this.handleClick(comment.id)}>
+                    删除
+                  </a>
                 </div>
                 <p className="user">
                   <span>{comment.name}</span>
